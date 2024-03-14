981203f1a9d14953c1d50d24601f641046099601
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2024-03-14 11:48:11 +0530

    update hero

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -3,7 +3,8 @@ import type { Locale } from "@/i18n.config";
 
 const dictionaries = {
   en: () => import("@/dictionaries/en.json").then((module) => module.default),
-  de: () => import("@/dictionaries/de.json").then((module) => module.default),
+  hi: () => import("@/dictionaries/hi.json").then((module) => module.default),
+  gu: () => import("@/dictionaries/gu.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        deviceSizes: [256,384,640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     },
 };
 
