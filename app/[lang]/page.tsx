e8b88fd99ed314e869c3c35e3dda880a093c45c0
kapish33 <kapishsingh2209@gmial.com>
2024-05-01 10:59:34 +0530

    Pages Changes

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,3 +1,4 @@
+import { ProfileForm } from '@/components/custum/UserForm';
 import Hero from '@/components/custum/hero';
 import { Locale } from '@/i18n.config';
 import { getDictionary } from '@/lib/dictionary';
@@ -14,10 +15,21 @@ export default async function Home({
   params: { lang: Locale };
 }) {
   const { page } = await getDictionary(lang);
+  const {
+    page: { form },
+  }: { page: { form: any } } = await getDictionary(lang);
 
   return (
     <section>
-      <Hero title={page.home.title} description={page.home.description} />
+      {/* <Hero title={page.home.title} description={page.home.description} /> */}
+      <div className='max-w-3xl mx-auto p-4'>
+      <ProfileForm
+        formData={form}
+        params={{
+          lang,
+        }}
+      />
+    </div>
     </section>
   );
 }
